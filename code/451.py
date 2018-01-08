4c0be127c429750f8cdc611eca992c011ac4c95a
RuidongZ <459597855@qq.com>
2018-01-08 16:29:51 +0800

    451. Sort Characters By Frequency

---
+++ code/451.py
@@ -0,0 +1,45 @@
+# -*- Encoding:UTF-8 -*-
+
+# 451. Sort Characters By Frequency
+
+# Given a string, sort it in decreasing order based on the frequency of characters.
+
+# Example 1:
+# Input: "tree"
+# Output: "eert"
+# 
+# Explanation:
+# 'e' appears twice while 'r' and 't' both appear once.
+# So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+#
+# Example 2:
+# Input: "cccaaa"
+# Output: "cccaaa"
+# 
+# Explanation:
+# Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+# Note that "cacaca" is incorrect, as the same characters must be together.
+#
+# Example 3:
+# Input: "Aabb"
+# Output: "bbAa"
+# 
+# Explanation:
+# "bbaA" is also a valid answer, but "Aabb" is incorrect.
+# Note that 'A' and 'a' are treated as two different characters.
+
+
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        d = {}
+        for i in s:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        ans = "".join(i[0] * i[1] for i in sorted(d.items(), key=lambda k: k[1], reverse=True))
+        return ans
