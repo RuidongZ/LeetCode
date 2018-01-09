3f1d0ef272521fc09ace1ee4777abdea9c9c7c33
RuidongZ <459597855@qq.com>
2018-01-09 17:55:45 +0800

    717. 1-bit and 2-bit Characters

---
+++ code/717.py
@@ -0,0 +1,50 @@
+# -*- Encoding:UTF-8 -*-
+
+# 717. 1-bit and 2-bit Characters
+
+# We have two special characters. The first character can be represented by one bit 0.
+# The second character can be represented by two bits (10 or 11).
+
+# Now given a string represented by several bits. Return whether the last character must be a one-bit character or not.
+# The given string will always end with a zero.
+#
+# Example 1:
+# Input: bits = [1, 0, 0]
+# Output: True
+# Explanation:
+# The only way to decode it is two-bit character and one-bit character. So the last character is one-bit character.
+
+# Example 2:
+# Input: bits = [1, 1, 1, 0]
+# Output: False
+# Explanation:
+# The only way to decode it is two-bit character and two-bit character. So the last character is NOT one-bit character.
+#
+# Note:
+# 1 <= len(bits) <= 1000.
+# bits[i] is always 0 or 1.
+
+
+class Solution(object):
+    def isOneBitCharacter(self, bits):
+        """
+        :type bits: List[int]
+        :rtype: bool
+        """
+        '''
+        # Better Solution
+        i = len(bits) - 2
+        while i >= 0 and bits[i] == 1:
+            i -= 1
+        return (len(bits) - i)% 2 ==0
+        '''
+        i = 0
+        ans = None
+        while i < len(bits):
+            if bits[i] == 1:
+                i += 2
+                ans = False
+            else:
+                i += 1
+                ans = True
+        return ans
