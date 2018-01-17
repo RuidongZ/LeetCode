55805b6dc8846a93aa14121bcdc475bb66dc93c6
RuidongZ <459597855@qq.com>
2018-01-17 11:34:29 +0800

    384. Shuffle an Array

---
+++ code/384.py
@@ -0,0 +1,53 @@
+# -*- Encoding:UTF-8 -*-
+
+# 384. Shuffle an Array
+
+# Shuffle a set of numbers without duplicates.
+
+# Example:
+#
+# // Init an array with set 1, 2, and 3.
+# int[] nums = {1,2,3};
+# Solution solution = new Solution(nums);
+#
+# // Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+# solution.shuffle();
+#
+# // Resets the array back to its original configuration [1,2,3].
+# solution.reset();
+#
+# // Returns the random shuffling of array [1,2,3].
+# solution.shuffle();
+
+
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.original = list(nums)
+        self.nums = list(nums)
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        self.nums = list(self.original)
+        return self.original
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        import random
+        random.shuffle(self.nums)
+        return self.nums
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
