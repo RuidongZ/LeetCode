ec970dabe81057376028851a4516eb387485eccc
RuidongZ <459597855@qq.com>
2018-02-02 16:35:58 +0800

    [*]729. My Calendar I

---
+++ code/729.py
@@ -0,0 +1,67 @@
+# -*- Encoding:UTF-8 -*-
+
+# 729. My Calendar I
+
+# Implement a MyCalendar class to store your events.
+# A new event can be added if adding the event will not cause a double booking.
+#
+# Your class will have the method, book(int start, int end).
+# Formally, this represents a booking on the half open interval [start, end),
+# the range of real numbers x such that start <= x < end.
+#
+# A double booking happens when two events have some non-empty intersection
+# (ie., there is some time that is common to both events.)
+#
+# For each call to the method MyCalendar.book, return true if the event can be added to the calendar successfully
+# without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+#
+# Your class will be called like this: MyCalendar cal = new MyCalendar(); MyCalendar.book(start, end)
+# Example 1:
+# MyCalendar();
+# MyCalendar.book(10, 20); // returns true
+# MyCalendar.book(15, 25); // returns false
+# MyCalendar.book(20, 30); // returns true
+# Explanation:
+# The first event can be booked.  The second can't because time 15 is already booked by another event.
+# The third event can be booked, as the first event takes every time less than 20, but not including 20.
+# Note:
+#
+# The number of calls to MyCalendar.book per test case will be at most 1000.
+# In calls to MyCalendar.book(start, end), start and end are integers in the range [0, 10^9].
+
+
+class MyCalendar(object):
+
+    def __init__(self):
+        self.calendar = [[-1, 0], [10**9, 10**9+1]]
+
+    def book(self, start, end):
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        '''
+        # Better Way
+        from bisect import bisect_left
+        i = bisect_left(self.events, (end,start))
+        if i<len(self.events) and self.events[i][-1] < end: return False
+        if i and self.events[i-1][0] > start: return False
+        self.events.insert(i, (end,start))
+        return True
+        '''
+        for i, c in enumerate(self.calendar[:-1]):
+            if start >= self.calendar[i+1][1]:
+                continue
+            elif start < self.calendar[i+1][0]:
+                if end <= self.calendar[i+1][0]:
+                    self.calendar.insert(i+1, [start, end])
+                    return True
+                else:
+                    return False
+            else:
+                return False
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
