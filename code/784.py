2554832b264f8259a78019d3685ce595aaf52b2e
RuidongZ <459597855@qq.com>
2018-03-05 11:24:32 +0800

    784. Letter Case Permutation

---
+++ code/784.py
@@ -0,0 +1,36 @@
+# -*- Encoding:UTF-8 -*-
+
+# 784. Letter Case Permutation
+
+# Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.
+# Return a list of all possible strings we could create.
+#
+# Examples:
+# Input: S = "a1b2"
+# Output: ["a1b2", "a1B2", "A1b2", "A1B2"]
+#
+# Input: S = "3z4"
+# Output: ["3z4", "3Z4"]
+#
+# Input: S = "12345"
+# Output: ["12345"]
+# Note:
+#
+# S will be a string with length at most 12.
+# S will consist only of letters or digits.
+
+
+class Solution(object):
+    def letterCasePermutation(self, S):
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+        ans = [""]
+        for s in S:
+            if s.isdigit():
+                ans = [i+s for i in ans]
+                continue
+            if s.isalpha():
+                ans = [i+j for i in ans for j in [s.upper(), s.lower()]]
+        return ans
