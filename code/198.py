a860145a54bd2a066ed356d90ab19f46ce980b2c
RuidongZ <459597855@qq.com>
2018-03-19 17:35:49 +0800

    [*]198. House Robber

---
+++ code/198.py
@@ -0,0 +1,33 @@
+# -*- Encoding:UTF-8 -*-
+
+# 198. House Robber
+
+# You are a professional robber planning to rob houses along a street.
+# Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them
+# is that adjacent houses have security system connected and it will automatically contact the police
+# if two adjacent houses were broken into on the same night.
+#
+# Given a list of non-negative integers representing the amount of money of each house,
+# determine the maximum amount of money you can rob tonight without alerting the police.
+
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Better Way
+        '''
+        prev, curr = 0, 0
+        for i in nums:
+            prev, curr = curr, max(curr, prev+i)
+
+        return curr
+        '''
+        dp = [0] * (len(nums)+2)
+
+        for i in range(len(nums)):
+            dp[i+2] = max(dp[i+1], dp[i]+nums[i])
+
+        return dp[-1]
