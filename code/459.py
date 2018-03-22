69992314f378a8fa3aa6ae35e011331e0e455064
RuidongZ <459597855@qq.com>
2018-03-22 21:46:36 +0800

    [***]459. Repeated Substring Pattern

---
+++ code/459.py
@@ -0,0 +1,36 @@
+# -*- Encoding:UTF-8 -*-
+
+# 459. Repeated Substring Pattern
+
+# Given a non-empty string check if it can be constructed by taking a substring of it
+# and appending multiple copies of the substring together.
+# You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+# Example 1:
+# Input: "abab"
+#
+# Output: True
+#
+# Explanation: It's the substring "ab" twice.
+# Example 2:
+# Input: "aba"
+#
+# Output: False
+# Example 3:
+# Input: "abcabcabcabc"
+#
+# Output: True
+#
+# Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+
+
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if not s:
+            return False
+
+        ss = (s + s)[1:-1]
+        return ss.find(s) != -1
