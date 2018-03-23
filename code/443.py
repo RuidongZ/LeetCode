343fea5bc88014c9ff02d828fa624f1113b0c9f1
RuidongZ <459597855@qq.com>
2018-03-23 10:53:38 +0800

    443. String Compression

---
+++ code/443.py
@@ -0,0 +1,81 @@
+# -*- Encoding:UTF-8 -*-
+
+# 443. String Compression
+
+# Given an array of characters, compress it in-place.
+#
+# The length after compression must always be smaller than or equal to the original array.
+#
+# Every element of the array should be a character (not int) of length 1.
+#
+# After you are done modifying the input array in-place, return the new length of the array.
+#
+#
+# Follow up:
+# Could you solve it using only O(1) extra space?
+#
+#
+# Example 1:
+# Input:
+# ["a","a","b","b","c","c","c"]
+#
+# Output:
+# Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+#
+# Explanation:
+# "aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+# Example 2:
+# Input:
+# ["a"]
+#
+# Output:
+# Return 1, and the first 1 characters of the input array should be: ["a"]
+#
+# Explanation:
+# Nothing is replaced.
+# Example 3:
+# Input:
+# ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+#
+# Output:
+# Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+#
+# Explanation:
+# Since the character "a" does not repeat, it is not compressed. "bbbbbbbbbbbb" is replaced by "b12".
+# Notice each digit has it's own entry in the array.
+# Note:
+# All characters have an ASCII value in [35, 126].
+# 1 <= len(chars) <= 1000.
+
+
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        count = 0
+        curchar = None
+        cur = 0
+
+        while cur < len(chars):
+            if count == 0:
+                curchar = chars[cur]
+                count += 1
+                cur += 1
+                continue
+
+            if curchar == chars[cur]:
+                count += 1
+                chars.pop(cur)
+            else:
+                if count > 1:
+                    for s in str(count):
+                        chars.insert(cur, s)
+                        cur += 1
+                count = 0
+        if count > 1:
+            for s in str(count):
+                chars.insert(cur, s)
+                cur += 1
+        return len(chars)
