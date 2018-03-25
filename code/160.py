d4ae5346116c00f886666d86839387ced21b3193
RuidongZ <459597855@qq.com>
2018-03-25 18:32:02 +0800

    [***]160. Intersection of Two Linked Lists

---
+++ code/160.py
@@ -0,0 +1,44 @@
+# -*- Encoding:UTF-8 -*-
+
+# 160. Intersection of Two Linked Lists
+
+# Write a program to find the node at which the intersection of two singly linked lists begins.
+#
+# For example, the following two linked lists:
+# A:          a1 → a2
+#                    ↘
+#                      c1 → c2 → c3
+#                    ↗
+# B:     b1 → b2 → b3
+#
+# begin to intersect at node c1.
+#
+#
+# Notes:
+#
+# If the two linked lists have no intersection at all, return null.
+# The linked lists must retain their original structure after the function returns.
+# You may assume there are no cycles anywhere in the entire linked structure.
+# Your code should preferably run in O(n) time and use only O(1) memory.
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return None
+
+        A, B = headA, headB
+        while A != B:
+            A = A.next if A else headB
+            B = B.next if B else headA
+        return A
