36d99aceff5cca81f90bdf1a547de25f9eca424c
RuidongZ <459597855@qq.com>
2018-03-25 18:21:57 +0800

    225. Implement Stack using Queues

---
+++ code/225.py
@@ -0,0 +1,73 @@
+# -*- Encoding:UTF-8 -*-
+
+# 225. Implement Stack using Queues
+
+# Implement the following operations of a stack using queues.
+#
+# push(x) -- Push element x onto stack.
+# pop() -- Removes the element on top of the stack.
+# top() -- Get the top element.
+# empty() -- Return whether the stack is empty.
+# Notes:
+# You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size,
+# and is empty operations are valid.
+# Depending on your language, queue may not be supported natively.
+# You may simulate a queue by using a list or deque (double-ended queue),
+# as long as you use only standard operations of a queue.
+# You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+
+
+class MyStack(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+        self.topNum = None
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: void
+        """
+        self.queue.append(x)
+        self.topNum = x
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        pre = None
+        tmp = []
+        while len(self.queue) > 1:
+            pre = self.queue.pop(0)
+            tmp.append(pre)
+        ans = self.queue.pop(0)
+        self.queue = tmp
+        self.topNum = pre
+        return ans
+
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        return self.topNum
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        return self.topNum is None
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
