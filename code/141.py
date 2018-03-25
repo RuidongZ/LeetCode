32837dcadcc870b31a250e7e5e74eb7c0716d721
RuidongZ <459597855@qq.com>
2018-03-25 17:50:41 +0800

    [*]141. Linked List Cycle

---
+++ code/141.py
@@ -0,0 +1,32 @@
+# -*- Encoding:UTF-8 -*-
+
+# 141. Linked List Cycle
+
+# Given a linked list, determine if it has a cycle in it.
+#
+# Follow up:
+# Can you solve it without using extra space?
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow:
+                return True
+        return False
